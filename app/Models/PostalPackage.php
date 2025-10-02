7db878f73cd2bfbd4561e989f0163736ef98c3b9
max.shirinzad@gmail.com <max.shirinzad@gmail.com>
2025-10-02 14:07:34 +0330

    PostalPackage model edit

--- app/Models/PostalPackage.php
+++ app/Models/PostalPackage.php
@@ -2,9 +2,41 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PostalPackage extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+    protected $fillable = [
+        'sender_id',
+        'receiver_id',
+        'weight',
+        'length',
+        'width',
+        'height',
+        'postage',
+        'tracking_code',
+    ];
+
+    protected $casts = [
+        'weight' => 'decimal:2',
+        'length' => 'decimal:2',
+        'width' => 'decimal:2',
+        'height' => 'decimal:2',
+    ];
+
+    public function sender(): BelongsTo
+    {
+        return $this->belongsTo(Person::class, 'sender_id');
+    }
+
+    public function receiver(): BelongsTo
+    {
+        return $this->belongsTo(Person::class, 'receiver_id');
+    }
+
+
 }
